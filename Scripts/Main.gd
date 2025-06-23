0a19b25834c9d54cbb029648cc57ab239aeb3801
Joseph Chu <hotcupofjoe2013@gmail.com>
2025-06-23 15:58:44 -0400

    chore: Added shooting bullet mechanics and relevant script

---
+++ Scripts/Main.gd
@@ -0,0 +1,9 @@
+extends Node
+
+@export var bullet:PackedScene
+var bullet_speed : int = 500
+
+func _on_fire_rate_timer_timeout() -> void:
+	var bullet_instance:Area2D = bullet.instantiate()
+	add_child(bullet_instance) 
+	bullet_instance.position = $Player.position
