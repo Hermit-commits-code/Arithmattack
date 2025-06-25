0e69a4169ada944312a9a22aace7741036d08d8d
Joseph Chu <hotcupofjoe2013@gmail.com>
2025-06-24 21:24:41 -0400

    chore Added Blocks Health

--- Scripts/Block.gd
+++ Scripts/Block.gd
@@ -1,10 +1,14 @@
 extends Control
 
 @export var colors:Array[Color]
+var health: int
 
 func _ready() -> void:
 #	Will return 0 - 3 randomly
 	$Background.color = colors[randi() % colors.size()]
-
+	health = randi_range(get_parent().min_blocks_health, get_parent().max_blocks_health)
+	$HealthLabel.text = str(health)
+	
 func _process(delta: float) -> void:
 	position.y += get_parent().blocks_speed * delta
+	
